362028faf15c6350440a1cd14d93f2b79f56eae5
Vinicius <vinicius.matheus252@gmail.com>
2018-10-06 23:14:26 -0300

    Add audios utils

--- utils/audioUtils.js
+++ utils/audioUtils.js
@@ -1,10 +1,11 @@
 const fs = require("fs");
 const Tree = require("../model/Tree");
+const Audio = require("../model/Audio");
 
 /**
  * Get all audios in the path as object
  * @param {String} path - path of the directory with the audios files
- * @returns {}
+ * @returns {Tree}
  */
 function getAudiosTree(path) {
   let tree = new Tree();
@@ -14,7 +15,7 @@ function getAudiosTree(path) {
 
 /**
  * Mount the tree of file recursively
- * @param {Object} tree - tree with the
+ * @param {Tree} tree - tree with the
  * @param {String} path - path of the directory with the audios files
  */
 function recursiveDirs(tree, path) {
@@ -45,4 +46,104 @@ function recursiveDirs(tree, path) {
   });
 }
 
-module.exports = { getAudiosTree };
+/**
+ * Search a audio in the list of audios
+ * @param {String} command - command in the message
+ * @param {Tree} audiosTress - audio's tree
+ * @returns {Audio} audio object ou null if not find
+ */
+function getAudioByCommand(command, audiosTree) {
+  return recursiveGetAudioByCommand(command, audiosTree);
+}
+
+/**
+ * Get the audio object in a list by a command
+ * @param {String} command - command in the message
+ * @param {Array<Tree>} audiosList - array with audios object
+ * @returns {Audio} audio object ou null if not find
+ */
+function getAudio(command, audiosList) {
+  let result = null;
+
+  if (audiosList.length !== 0) {
+    audiosList.forEach(audio => {
+      if (audio.command === command) {
+        result = audio;
+      }
+    });
+  }
+
+  return result;
+}
+
+/**
+ * Search a audio in the audio's tree recursively
+ * @param {String} command - command in the message
+ * @param {Tree} audiosTrees - audio's tree
+ * @returns {Audio} audio object ou null if not find
+ */
+function recursiveGetAudioByCommand(command, audiosTree) {
+  let audio = null;
+
+  // check if any audio with the command
+  if (audiosTree.files) {
+    audio = getAudio(command, audiosTree.files);
+  }
+
+  // if no find a audio
+  if (!audio && audiosTree.dirs) {
+    // search in the directories
+    Object.keys(audiosTree.dirs).forEach(dir => {
+      let audioInDir = recursiveGetAudioByCommand(
+        command,
+        audiosTree.dirs[dir]
+      );
+      if (audioInDir && !audio) {
+        audio = audioInDir;
+      }
+    });
+  }
+
+  return audio;
+}
+
+/**
+ * Send the audio to the voice channel
+ * @param {Object} voiceChannel - user's voice channel
+ * @param {object} audio - audio object
+ * @param {Object} server - server context
+ */
+function sendAudio(voiceChannel, audio, server) {
+  // connect to de server
+  if (server.getConnection() === null) {
+    // enter in the voice channel
+    voiceChannel.join().then(connection => {
+      var dispatcher;
+      // send the audio
+      if (audio.type === "ogg") {
+        dispatcher = connection.playStream(fs.createReadStream(audio.file), {
+          type: audio.type
+        });
+      } else {
+        dispatcher = connection.playStream(audio.file);
+      }
+
+      // end the connection
+      dispatcher.on("end", () => {
+        connection.disconnect();
+        voiceChannel.leave();
+      });
+    });
+  } else {
+    // if is connected
+    if (audio.type === "ogg") {
+      server.getConnection().playStream(fs.createReadStream(audio.file), {
+        type: audio.type
+      });
+    } else {
+      server.getConnection().playStream(audio.file);
+    }
+  }
+}
+
+module.exports = { getAudiosTree, getAudioByCommand, sendAudio };
