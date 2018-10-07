96e8cc0ca8aa95773ed38df348f5d3467bc835ff
Vinicius <vinicius.matheus252@gmail.com>
2018-10-06 23:14:51 -0300

    Update default messages

--- config/defaultMessages.js
+++ config/defaultMessages.js
@@ -2,8 +2,9 @@ module.exports = {
   busyMessage: "Desculpa, estou ocupado em outro canal de voz!",
   goToVoiceChannelMessage: "Entre em algum canal de voz!",
   permissionDeniedMessage: "Você não tem permissão!",
-  permissionAddMessage: "Permissão concedida para: ",
-  permissionRemoveMessage: "Permissão removida para: ",
-  listPermissionsMessage: "Permissões: ",
-  nonePermissionMessage: "Nenhuma permissão adicionada!"
+  permissionAddMessage: "Permissão concedida para:",
+  permissionRemoveMessage: "Permissão removida para:",
+  listPermissionsMessage: "Permissões:",
+  nonePermissionMessage: "Nenhuma permissão adicionada!",
+  listenningMessage: "Digite !meajuda"
 };
